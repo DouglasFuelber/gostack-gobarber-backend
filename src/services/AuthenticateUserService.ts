922c4cbb40d3122e7d25e7227503ee8be4f54909
Douglas Fuelber <contato@douglasfuelber.com>
2020-04-18 17:25:43 -0300

    JWT token generator on session creation

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,7 +29,12 @@ class AuthenticateUserService {
     if (!passwordMatched)
       throw new Error('Incorrect email/password combination.');
 
-    return { user };
+    const token = sign({}, '0e56764ff29ce827c97bbc3ee44c2174', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 export default AuthenticateUserService;
