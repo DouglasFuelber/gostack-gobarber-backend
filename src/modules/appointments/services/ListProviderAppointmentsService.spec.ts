bfe486bd29a119326dbab4b2419304f30413f951
Douglas Fuelber <contato@douglasfuelber.com>
2020-05-14 20:56:55 -0300

    List provider appointments

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,43 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('should be able to list provider appointments in day', async () => {
+    const ap1 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'client',
+      date: new Date(2020, 8, 20, 14, 0, 0),
+    });
+
+    const ap2 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'client',
+      date: new Date(2020, 8, 20, 10, 0, 0),
+    });
+
+    const ap3 = await fakeAppointmentsRepository.create({
+      provider_id: 'provider',
+      user_id: 'client',
+      date: new Date(2020, 8, 20, 15, 0, 0),
+    });
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      year: 2020,
+      month: 9,
+      day: 20,
+    });
+
+    expect(appointments).toEqual([ap1, ap2, ap3]);
+  });
+});
